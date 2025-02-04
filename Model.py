10b5480bdefc6187e2bc1d1bf4d6a27d7e804fed
OliverErikAnderzen <olivererikanderzen@gmail.com>
2025-02-04 20:10:52 +0100

    Create set_game_over() in Model.py

--- Model.py
+++ Model.py
@@ -2,3 +2,8 @@ class Model:
     def __init__(self, data):
         self.cell_size = data["cell_size"]
         self.grid_size = data["grid_size"]
+        self.game_over = False
+
+    def set_game_over(self, new_status):
+        if new_status == True or new_status == False:
+            self.game_over = new_status
