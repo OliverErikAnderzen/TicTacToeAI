96b723ff0e4f7f04e0df1a1a4bffe90f4cd6859b
OliverErikAnderzen <olivererikanderzen@gmail.com>
2025-02-04 20:14:40 +0100

    Update __init__() in View.py

--- View.py
+++ View.py
@@ -3,6 +3,13 @@ import pygame
 class View:
     def __init__(self, model):
         self.model = model
+        self.grid_size = model.grid_size
+        self.cell_size = model.cell_size
+        self.screen = pygame.display.set_mode(
+            (self.grid_size[0] * self.cell_size, self.grid_size[1] * self.cell_size)
+        )
+        pygame.display.set_caption("TicTacToeAI")
+
     def draw_segment(self, x, y, color):
         cs = self.cell_size
         pygame.draw.rect(self.screen, color, (cs*x, cs*y, cs, cs))
