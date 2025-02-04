16debf256be49e8362d381b6511a219ff45e3120
OliverErikAnderzen <olivererikanderzen@gmail.com>
2025-02-04 20:12:16 +0100

    Create run() in Controller.py

--- Controller.py
+++ Controller.py
@@ -2,3 +2,9 @@ class Controller:
     def __init__(self, model, view):
         self.model = model
         self.view = view
+    def run(self):
+        while not self.model.game_over:
+            self.view.render()
+            action = self.get_player_input()
+            if action == "QUIT":
+                self.model.set_game_over(True)
