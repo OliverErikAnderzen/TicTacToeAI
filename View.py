3541976fda79e07ab92446967118a9f05b44d11d
OliverErikAnderzen <olivererikanderzen@gmail.com>
2025-02-04 20:14:16 +0100

    Create draw_segment() in View.py

--- View.py
+++ View.py
@@ -3,6 +3,9 @@ import pygame
 class View:
     def __init__(self, model):
         self.model = model
+    def draw_segment(self, x, y, color):
+        cs = self.cell_size
+        pygame.draw.rect(self.screen, color, (cs*x, cs*y, cs, cs))
 
     def render(self):
         grid_size = self.model.grid_size
