2d4c19aeebe3860dcd96a4ba102622654020e450
OliverErikAnderzen <olivererikanderzen@gmail.com>
2025-02-04 19:31:38 +0100

    Create render() in View.py

--- View.py
+++ View.py
@@ -3,5 +3,7 @@ import pygame
 class View:
     def __init__(self, model):
         self.model = model
-        self.grid_size = model.grid_size
-        self.cell_size = model.cell_size
+
+    def render(self):
+        grid_size = self.model.grid_size
+        cell_size = self.model.cell_size
